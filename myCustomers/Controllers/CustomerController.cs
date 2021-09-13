3632ce94bcd56da9cce600cbe4f9b06b48d31e5c
samuel borges <muka_brayan-borges@hotmail.com>
2021-09-13 13:29:13 -0300

    #2 - Customer list beggins

---
+++ myCustomers/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myCustomers.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ILogger<CustomerController> _logger;
+
+        public CustomerController(ILogger<CustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CustomerList()
+        {
+            using (myclientsContext entity = new myclientsContext())
+            {
+                List<Customer> lista = entity.Customer.ToList();
+                lista = lista.OrderByDescending(u => u.CdCustomer).ToList();
+                ViewBag.Customer = lista.Take(10).ToList();
+            }
+
+            return View();
+        }
+
+        public IList<Customer> ListarClientes()
+        {
+            return null;
+        }
+
+    }
+}
--- myCustomers/EntityMigrations/myclientsContext.cs
+++ myCustomers/EntityMigrations/myclientsContext.cs
@@ -23,7 +23,7 @@ namespace myCustomers
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                
+                optionsBuilder.UseMySQL("server=localhost;user id=root;Pwd=33821049;persistsecurityinfo=True;database=myclients");
             }
         }
 
